2b52fe5aab74e04da0109ccc749b8755ee9fcbe6
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-21 22:20:58 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -11,13 +11,12 @@ def find_element_index(array, value_to_find)
   found_value_index
 end
 
-def find_element_index(array, value_to_find)
-  count = 0
-  while count < array.length do
-    if array[count] == value_to_find
-      return count
-    end
-    count += 1
-  end
-end
-This solution drops the extr
+# def find_element_index(array, value_to_find)
+#   count = 0
+#   while count < array.length do
+#     if array[count] == value_to_find
+#       return count
+#     end
+#     count += 1
+#   end
+# end
